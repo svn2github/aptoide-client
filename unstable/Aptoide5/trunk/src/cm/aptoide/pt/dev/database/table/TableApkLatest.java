cf1b9828e144018395f648c09d766802c790f124
rui.mateus <rui.mateus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-10-09 15:13:30 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1427 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoide5/trunk/src/cm/aptoide/pt/dev/database/table/TableApkLatest.java
@@ -0,0 +1,23 @@
+package cm.aptoide.pt.dev.database.table;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.Statement;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rmateus
+ * Date: 02-10-2013
+ * Time: 16:01
+ * To change this template use File | Settings | File Templates.
+ */
+public class TableApkLatest {
+
+
+    public final static String COLUMN_APK_ID = "id_apk";
+    public final static String COLUMN_REPO_ID = "id_repo";
+
+
+
+
+}
---
+++ unstable/Aptoide5/trunk/testing/src/Main.java
@@ -0,0 +1,37 @@
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rmateus
+ * Date: 03-10-2013
+ * Time: 16:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+    ArrayList<Integer> integers = new ArrayList<Integer>();
+
+    public static void main(String[] args) throws MalformedURLException {
+        Main main = new Main();
+        main.execute();
+
+    }
+
+    private void execute() {
+
+
+
+    }
+
+}
