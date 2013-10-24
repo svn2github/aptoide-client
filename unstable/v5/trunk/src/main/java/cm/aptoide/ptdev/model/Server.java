365e2b9842eee693bb2b7fa69527068798a7008d
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-10-24 12:56:39 +0000

    Webservices tested
    git-svn-id: http://aptoide.org/repo/aptoide-client@1461 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- unstable/v5/trunk/src/main/java/cm/aptoide/ptdev/model/Server.java
+++ unstable/v5/trunk/src/main/java/cm/aptoide/ptdev/model/Server.java
@@ -9,8 +9,35 @@ package cm.aptoide.ptdev.model;
  */
 public abstract class Server {
 
+    private String name;
+    private String url;
+    private String hash;
 
+    public String getName() {
+        return name;
+    }
 
+    public Server setName(String name) {
+        this.name = name;
+        return this;
+    }
 
+    public String getUrl() {
+        return url;
+    }
+
+    public Server setUrl(String url) {
+        this.url = url;
+        return this;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public Server setHash(String hash) {
+        this.hash = hash;
+        return this;
+    }
 
 }
