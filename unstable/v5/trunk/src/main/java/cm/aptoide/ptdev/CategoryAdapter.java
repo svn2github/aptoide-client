9585be0cd4bc08732e4bcbb88cf35564b6d51bf9
rui.mateus <rui.mateus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-11-19 17:16:15 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1489 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- unstable/v5/trunk/src/main/java/cm/aptoide/ptdev/CategoryAdapter.java
+++ unstable/v5/trunk/src/main/java/cm/aptoide/ptdev/CategoryAdapter.java
@@ -2,13 +2,14 @@ package cm.aptoide.ptdev;
 
 
 import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 
@@ -19,53 +20,84 @@ import java.util.ArrayList;
  * Time: 16:00
  * To change this template use File | Settings | File Templates.
  */
-public class CategoryAdapter extends BaseAdapter {
+public class CategoryAdapter extends CursorAdapter {
 
 
     private final Context context;
     ArrayList<Category> items;
 
-    public CategoryAdapter(Context context, ArrayList<Category> categories) {
+    public CategoryAdapter(Context context) {
+        super(context, null, FLAG_REGISTER_CONTENT_OBSERVER);
         this.context = context;
-        items = categories;
-
     }
 
-    @Override
-    public int getCount() {
-        return items.size();
-    }
+
 
     @Override
-    public Category getItem(int position) {
-        return items.get(position);
-    }
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
 
-    @Override
-    public long getItemId(int position) {
-        return getItem(position).getId();
-    }
+        View v = null;
+        int type = getItemViewType(cursor);
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+        switch (type){
+            case 0:
+                v = LayoutInflater.from(context).inflate(R.layout.row_app, null);
 
-        View v = LayoutInflater.from(context).inflate(R.layout.row_item_category_first_level_list, null);
-
-        ((TextView)v.findViewById(R.id.category_first_level_name)).setText(getItem(position).getName());
-        ((TextView)v.findViewById(R.id.category_first_level_number)).setText(String.valueOf(getItem(position).getAppsNumber()));
-        ImageView icon = (ImageView) v.findViewById(R.id.category_first_level_icon);
-        if(getItem(position).getName().equals("Applications")){
-            icon.setImageResource(R.drawable.cat_applications);
-        } else if(getItem(position).getName().equals("Games")){
-            icon.setImageResource(R.drawable.cat_games);
-        } else if(getItem(position).getName().equals("Top Apps")){
-            icon.setImageResource(R.drawable.cat_top_apps);
-        } else if(getItem(position).getName().equals("Latest Apps")){
-            icon.setImageResource(R.drawable.cat_latest);
-        } else {
-            icon.setImageResource(R.drawable.cat_custom);
+                break;
+            case 1:
+                v = LayoutInflater.from(context).inflate(R.layout.row_item_category_first_level_list, null);
+                break;
         }
 
         return v;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        Cursor c = (Cursor) getItem(position);
+        return getItemViewType(c);
+    }
+
+    private int getItemViewType(Cursor cursor){
+        return cursor.getInt(cursor.getColumnIndex("type"));
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 2;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        String name = cursor.getString(cursor.getColumnIndex("name"));
+        int count = cursor.getInt(cursor.getColumnIndex("count"));
+        int type = getItemViewType(cursor);
+
+
+        switch (type) {
+
+            case 0:
+                ((TextView) view.findViewById(R.id.app_name)).setText(name);
+                break;
+            case 1:
+                ImageView icon = (ImageView) view.findViewById(R.id.category_first_level_icon);
+                ((TextView) view.findViewById(R.id.category_first_level_name)).setText(name);
+                ((TextView) view.findViewById(R.id.category_first_level_number)).setText(String.valueOf(count));
+                if (name.equals("Applications")) {
+                    icon.setImageResource(R.drawable.cat_applications);
+                } else if (name.equals("Games")) {
+                    icon.setImageResource(R.drawable.cat_games);
+                } else if (name.equals("Top Apps")) {
+                    icon.setImageResource(R.drawable.cat_top_apps);
+                } else if (name.equals("Latest Apps")) {
+                    icon.setImageResource(R.drawable.cat_latest);
+                } else {
+                    icon.setImageResource(R.drawable.cat_custom);
+                }
+                break;
+
+
+        }
     }
 }
