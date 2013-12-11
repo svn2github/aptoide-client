371836816ec07b10fd5582f9c3f8dd414a12e570
teresa.deus <teresa.deus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-12-11 15:49:33 +0000

    settings, about, navigation drawer
    git-svn-id: http://aptoide.org/repo/aptoide-client@1539 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoidev5/trunk/v5engine/src/main/java/cm/aptoide/ptdev/RollbackActivity.java
@@ -0,0 +1,36 @@
+package cm.aptoide.ptdev;
+
+import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
+
+
+public class RollbackActivity extends SherlockActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_rollback);
+
+
+        getSupportActionBar().setTitle(getString(R.string.excluded_updates));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int i = item.getItemId();
+
+        if (i == android.R.id.home) {
+            finish();
+        } else if (i == R.id.abs__home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
