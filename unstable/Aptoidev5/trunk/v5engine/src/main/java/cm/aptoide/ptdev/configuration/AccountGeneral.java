f47083bd30b797cf232914bbe751beb4cb83e2df
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2013-12-12 15:00:35 +0000

    Account General information
    git-svn-id: http://aptoide.org/repo/aptoide-client@1543 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoidev5/trunk/v5engine/src/main/java/cm/aptoide/ptdev/configuration/AccountGeneral.java
@@ -0,0 +1,11 @@
+package cm.aptoide.ptdev.configuration;
+
+/**
+ * Created by brutus on 10-12-2013.
+ */
+public class AccountGeneral {
+
+    public static final String ACCOUNT_TYPE = "cm.aptoide.ptdev";
+
+    public static final String AUTHTOKEN_TYPE = "Access to an Aptoide account";
+}
