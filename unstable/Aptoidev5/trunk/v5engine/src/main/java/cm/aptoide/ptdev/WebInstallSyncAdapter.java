49ed2a875a4fdb9a057b3bf769b1d296c72ee964
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-01-27 15:46:05 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1628 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoidev5/trunk/v5engine/src/main/java/cm/aptoide/ptdev/WebInstallSyncAdapter.java
@@ -0,0 +1,55 @@
+package cm.aptoide.ptdev;
+
+import android.accounts.Account;
+import android.content.AbstractThreadedSyncAdapter;
+import android.content.ContentProviderClient;
+import android.content.Context;
+import android.content.SyncResult;
+import android.os.Bundle;
+import com.rabbitmq.client.*;
+import com.rabbitmq.client.impl.AMQConnection;
+import com.rabbitmq.client.impl.ChannelN;
+
+import java.io.IOException;
+
+/**
+ * Created by j-pac on 27-01-2014.
+ */
+public class WebInstallSyncAdapter extends AbstractThreadedSyncAdapter {
+
+    public WebInstallSyncAdapter(Context context, boolean autoInitialize) {
+        super(context, autoInitialize);
+    }
+
+
+    @Override
+    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(extras.getString("host"));
+        factory.setConnectionTimeout(20000);
+
+
+        try {
+            Connection connection = factory.newConnection();
+            Channel channel = connection.createChannel();
+            channel.basicQos(0);
+
+            GetResponse response = channel.basicGet(extras.getString("queueId"), false);
+
+            if (response != null) {
+                String message = new String(response.getBody(), "UTF-8");
+                handleMessage(message);
+                channel.basicAck(response.getEnvelope().getDeliveryTag(), false);
+            }
+
+            connection.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+}
