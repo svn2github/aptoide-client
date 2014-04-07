8062ccb0b0025b5c0fab7cae9b0162d212fe6ea4
rui.mateus <rui.mateus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-04-07 10:55:26 +0000

    fix commit
    git-svn-id: http://aptoide.org/repo/aptoide-client@1833 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- 5.0.0/trunk/v5engine/src/main/java/cm/aptoide/ptdev/EnumDownloadStates.java
+++
@@ -1,8 +0,0 @@
-package cm.aptoide.ptdev;
-
-/**
- * Created by tdeus on 2/7/14.
- */
-public enum EnumDownloadStates {
-    ACTIVE, COMPLETE, PENDING, INACTIVE, ERROR;
-}
--- 5.0.0/trunk/v5engine/src/main/java/cm/aptoide/ptdev/events/AppViewRefresh.java
+++
@@ -1,7 +0,0 @@
-package cm.aptoide.ptdev.events;
-
-/**
- * Created by rmateus on 27-12-2013.
- */
-public class AppViewRefresh {
-}
