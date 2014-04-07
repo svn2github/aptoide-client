b1074f389690319d5ab9e6a7dd286ca8ef775f1e
rui.mateus <rui.mateus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-04-07 10:50:49 +0000

    fix commit
    git-svn-id: http://aptoide.org/repo/aptoide-client@1831 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- 5.0.0/trunk/v5engine/src/main/java/cm/aptoide/ptdev/DownloadInterface.java
+++
@@ -1,9 +0,0 @@
-package cm.aptoide.ptdev;
-
-/**
- * Created by rmateus on 07-02-2014.
- */
-public interface DownloadInterface {
-
-    public void installApp(long id);
-}
--- 5.0.0/trunk/v5engine/src/main/java/cm/aptoide/ptdev/DownloadServiceConnected.java
+++
@@ -1,7 +0,0 @@
-package cm.aptoide.ptdev;
-
-/**
- * Created by rmateus on 13-12-2013.
- */
-public class DownloadServiceConnected {
-}
