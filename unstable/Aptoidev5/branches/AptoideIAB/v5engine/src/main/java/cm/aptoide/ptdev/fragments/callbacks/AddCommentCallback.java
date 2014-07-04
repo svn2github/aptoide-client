8e64067139bcad3c18006a54645672bce1614b83
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-07-04 19:25:12 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1930 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/fragments/callbacks/AddCommentCallback.java
@@ -0,0 +1,8 @@
+package cm.aptoide.ptdev.fragments.callbacks;
+
+/**
+ * Created by jcosta on 02-07-2014.
+ */
+public interface AddCommentCallback {
+    void addComment(String comment, String answerTo);
+}
