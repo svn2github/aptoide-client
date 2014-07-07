047ddf77582163b561f791c1566ce0c1c7735972
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-07-07 10:09:12 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1932 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/webservices/Errors.java
+++ unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/webservices/Errors.java
@@ -17,6 +17,8 @@ public class Errors {
         errors.put("IARG-2", R.string.error_IARG_2);
         errors.put("IARG-3", R.string.error_IARG_3);
         errors.put("IARG-4", R.string.error_IARG_4);
+        errors.put("IARG-5", R.string.error_IARG_5);
+        errors.put("IARG-6", R.string.error_IARG_6);
         errors.put("IARG-100", R.string.error_IARG_100);
         errors.put("IARG-101", R.string.error_IARG_101);
         errors.put("IARG-102", R.string.error_IARG_102);
@@ -41,7 +43,11 @@ public class Errors {
         errors.put("MARG-4", R.string.error_MARG_4);
         errors.put("MARG-5", R.string.error_MARG_5);
         errors.put("MARG-6", R.string.error_MARG_6);
-        errors.put("MARG-6", R.string.error_MARG_6);
+        errors.put("MARG-7", R.string.error_MARG_7);
+        errors.put("MARG-8", R.string.error_MARG_8);
+        errors.put("MARG-9", R.string.error_MARG_9);
+        errors.put("MARG-10", R.string.error_MARG_10);
+        errors.put("MARG-11", R.string.error_MARG_11);
         errors.put("MARG-100", R.string.error_MARG_100);
         errors.put("MARG-101", R.string.error_MARG_101);
         errors.put("MARG-102", R.string.error_MARG_102);
@@ -50,6 +56,12 @@ public class Errors {
         errors.put("MARG-105", R.string.error_MARG_105);
         errors.put("MARG-106", R.string.error_MARG_106);
         errors.put("MARG-107", R.string.error_MARG_107);
+        errors.put("MARG-108", R.string.error_MARG_108);
+        errors.put("MARG-109", R.string.error_MARG_109);
+        errors.put("MARG-110", R.string.error_MARG_110);
+        errors.put("MARG-111", R.string.error_MARG_111);
+        errors.put("MARG-112", R.string.error_MARG_112);
+        errors.put("MARG-113", R.string.error_MARG_113);
         errors.put("MARG-200", R.string.error_MARG_200);
         errors.put("MARG-201", R.string.error_MARG_201);
         errors.put("MARG-202", R.string.error_MARG_202);
@@ -70,6 +82,10 @@ public class Errors {
         errors.put("WOP-5", R.string.error_WOP_5);
         errors.put("WOP-6", R.string.error_WOP_6);
         errors.put("WOP-7", R.string.error_WOP_7);
+        errors.put("WOP-8", R.string.error_WOP_8);
+        errors.put("WOP-9", R.string.error_WOP_9);
+        errors.put("WOP-10", R.string.error_WOP_10);
+        errors.put("WOP-11", R.string.error_WOP_11);
 
         errors.put("REPO-1", R.string.error_REPO_1);
         errors.put("REPO-2", R.string.error_REPO_2);
