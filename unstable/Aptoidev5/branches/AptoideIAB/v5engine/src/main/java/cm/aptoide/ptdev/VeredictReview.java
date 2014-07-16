1402fc8ffe9e44855ccf40aa645e10ffec2d43ac
jcosta <jcosta@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-07-16 17:22:54 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@1955 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/VeredictReview.java
@@ -0,0 +1,28 @@
+package cm.aptoide.ptdev;
+
+import java.util.HashMap;
+import java.util.Locale;
+
+/**
+ * Created by jcosta on 16-07-2014.
+ */
+
+public enum VeredictReview {
+    GOOD(R.string.flag_review_good), LICENSE(R.string.flag_license), FAKE(R.string.flag_review_fake), FREEZE(R.string.flag_review_freeze), VIRUS(R.string.flag_review_virus);
+
+    private int string;
+
+    VeredictReview (int string){
+
+        this.string = string;
+    }
+
+    public int getString() {
+        return string;
+    }
+
+    public static VeredictReview reverseLookup (String string) {
+        return VeredictReview.valueOf(string.toUpperCase(Locale.ENGLISH) );
+    }
+}
+
