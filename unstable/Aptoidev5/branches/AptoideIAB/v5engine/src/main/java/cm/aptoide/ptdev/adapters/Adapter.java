e742dbd1ae7c44ff2edc9f5d23b5fcc5af490f1e
teresa.deus <teresa.deus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-07-21 09:30:09 +0000

    home adapter
    git-svn-id: http://aptoide.org/repo/aptoide-client@1969 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/adapters/Adapter.java
+++ unstable/Aptoidev5/branches/AptoideIAB/v5engine/src/main/java/cm/aptoide/ptdev/adapters/Adapter.java
@@ -3,9 +3,11 @@ package cm.aptoide.ptdev.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.database.DataSetObserver;
+import android.support.v7.widget.PopupMenu;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -14,7 +16,9 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.flurry.android.FlurryAgent;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
@@ -22,6 +26,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import cm.aptoide.ptdev.Aptoide;
+import cm.aptoide.ptdev.DownloadInterface;
+import cm.aptoide.ptdev.EnumCategories;
 import cm.aptoide.ptdev.MoreEditorsChoiceActitivy;
 import cm.aptoide.ptdev.MoreTopAppsActivity;
 import cm.aptoide.ptdev.R;
@@ -52,6 +58,7 @@ public class Adapter extends BaseAdapter {
 
     Set<Integer> set = new HashSet<Integer>();
     int maxCount;
+
     public Adapter(Context context) {
         this.context = context;
         float screenWidth = getScreenWidthInDip();
@@ -79,7 +86,7 @@ public class Adapter extends BaseAdapter {
                     set.add(count);
                 }
 
-                //Log.d("yuuup", set + " maxcount is: " + maxCount);
+                Log.d("yuuup", set + " maxcount is: " + maxCount);
 
             }
         });
@@ -120,7 +127,7 @@ public class Adapter extends BaseAdapter {
             result= getItem(position).getItemsSize() + 1 ;
         }
 
-        //Log.d("yuuuup", "itemType" + result);
+//        Log.d("yuuuup", "itemType" + result);
 
         return result;
 
@@ -155,7 +162,7 @@ public class Adapter extends BaseAdapter {
     public View getView(final int position, View convertView, ViewGroup parent) {
 
         View item = null;
-        //Log.d("yuuup", "cenas");
+//        Log.d("yuuup", "cenas");
         switch (getItemViewType(position)){
 
             case 1:
@@ -210,7 +217,7 @@ public class Adapter extends BaseAdapter {
 
         View view;
         if(convertView==null){
-            //Log.d("Sup", "Init view " );
+//            Log.d("Sup", "Init view " );
             view = new LinearLayout(context);
             LinearLayout layout = (LinearLayout) view;
             layout.setOrientation(LinearLayout.HORIZONTAL);
@@ -227,7 +234,7 @@ public class Adapter extends BaseAdapter {
 
 
         }else{
-            //Log.d("Sup", "Reusing view ");
+//            Log.d("Sup", "Reusing view ");
             view = convertView;
         }
 
@@ -241,6 +248,8 @@ public class Adapter extends BaseAdapter {
                 holder = new CellViewHolder();
                 holder.tv = (TextView) (root.findViewById(R.id.app_name));
                 holder.icon = (ImageView) (root.findViewById(R.id.app_icon));
+                holder.category = (TextView) (root.findViewById(R.id.app_category));
+                holder.overflow = (ImageView) (root.findViewById(R.id.ic_action));
                 root.setTag(holder);
             }
 
@@ -256,12 +265,29 @@ public class Adapter extends BaseAdapter {
                     Intent i = new Intent(context, appViewClass);
                     long id = item.getId();
                     i.putExtra("id", id);
+                    i.putExtra("download_from", "home_page");
                     context.startActivity(i);
                 }
             });
             ImageLoader.getInstance().displayImage(icon, holder.icon);
             holder.tv.setText(item.getName());
 
+            String category;
+            try {
+                int cat = Integer.parseInt(item.getCategory());
+                category = context.getString(EnumCategories.getCategoryName(cat));
+            }catch (Exception e){
+                category = item.getCategoryString();
+            }
+            holder.category.setText(category);
+
+            holder.overflow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FlurryAgent.logEvent("Home_Page_Opened_Popup_Menu");
+                    showPopup(v, item.getId());
+                }
+            });
         }
 
         return view;
@@ -275,6 +301,46 @@ public class Adapter extends BaseAdapter {
     public static class CellViewHolder{
         public TextView tv;
         public ImageView icon;
+        public TextView category;
+        public ImageView overflow;
     }
+
+    public void showPopup(View v, long id) {
+        PopupMenu popup = new PopupMenu(context, v);
+        popup.setOnMenuItemClickListener(new MenuListener(context, id));
+        popup.inflate(R.menu.menu_actions);
+        popup.show();
+    }
+
+    static class MenuListener implements PopupMenu.OnMenuItemClickListener{
+
+        Context context;
+        long id;
+
+        MenuListener(Context context, long id) {
+            this.context = context;
+            this.id = id;
+
+
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            int i = menuItem.getItemId();
+
+            if (i == R.id.menu_install) {
+                ((DownloadInterface)context).installApp(id);
+                Toast.makeText(context, context.getString(R.string.starting_download), Toast.LENGTH_LONG).show();
+                FlurryAgent.logEvent("Home_Page_Clicked_Install_From_Popup_Menu");
+                return true;
+            } else if (i == R.id.menu_schedule) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+
 
 }
