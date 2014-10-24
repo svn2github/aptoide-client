8e8d690de61987d963ba9553a9bda08556bb8366
teresa.deus <teresa.deus@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-10-24 15:06:34 +0000

    timeline invite friends
    git-svn-id: http://aptoide.org/repo/aptoide-client@2234 8d55eb69-6847-4b75-98a7-935b3913b0ec

--- unstable/Aptoidev5/branches/Aptoide5.2.1/new_branch/v5engine/src/main/java/cm/aptoide/ptdev/TimeLineFriendsInviteActivity.java
+++ unstable/Aptoidev5/branches/Aptoide5.2.1/new_branch/v5engine/src/main/java/cm/aptoide/ptdev/TimeLineFriendsInviteActivity.java
@@ -6,30 +6,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.GridView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.manuelpeinado.multichoiceadapter.extras.actionbarcompat.MultiChoiceArrayAdapter;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.octo.android.robospice.SpiceManager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import cm.aptoide.ptdev.adapters.InviteFriendsListAdapter;
 import cm.aptoide.ptdev.fragments.GenericResponse;
 import cm.aptoide.ptdev.services.HttpClientSpiceService;
-import cm.aptoide.ptdev.webservices.json.TimelineActivityJson;
 import cm.aptoide.ptdev.webservices.timeline.ListUserFriendsRequest;
 import cm.aptoide.ptdev.webservices.timeline.RegisterUserFriendsInviteRequest;
 import cm.aptoide.ptdev.webservices.timeline.TimelineRequestListener;
@@ -43,39 +40,36 @@ public class TimeLineFriendsInviteActivity extends ActionBarActivity {
     private TextView friends_using_timeline;
     private TextView friends_to_invite;
     private LinearLayout friends_list;
+    private ListView listView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Aptoide.getThemePicker().setAptoideTheme(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_timeline_invite_friends);
+        listView = getListView();
 
         friends_list = (LinearLayout) findViewById(R.id.friends_list);
         friends_using_timeline = (TextView) findViewById(R.id.friends_using_timeline);
         friends_to_invite = (TextView) findViewById(R.id.friends_to_invite);
-
+        View footer_friends_to_invite = LayoutInflater.from(this).inflate(R.layout.footer_invite_friends, null);
+        listView.addFooterView(footer_friends_to_invite);
+        listView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
         rebuildList(savedInstanceState);
-        View invite =findViewById(R.id.timeline_invite);
+        Button invite = (Button) footer_friends_to_invite.findViewById(R.id.timeline_invite);
         final Context c = this;
         invite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-
-
                 RegisterUserFriendsInviteRequest request = new RegisterUserFriendsInviteRequest();
-                for(long id : adapter.getCheckedItems()){
-                    request.addEmail(adapter.getItem((int)id).getEmail());
+                for(long id : listView.getCheckItemIds()){
+                    Log.d("pois","id:"+id);
+                    Log.d("pois","id:"+adapter.getItem((int) id).getEmail());
+                    request.addEmail(adapter.getItem((int) id).getEmail());
                 }
-
-
                 manager.execute(request,new TimelineRequestListener<GenericResponse>(){
-
-                    @Override
-                    protected void caseFAIL() {
-
-                    }
-
                     @Override
                     protected void caseOK(GenericResponse response) {
                         Toast.makeText(c, c.getString(R.string.facebook_timeline_friends_invited), Toast.LENGTH_LONG).show();
@@ -107,28 +101,24 @@ public class TimeLineFriendsInviteActivity extends ActionBarActivity {
 
     SpiceManager manager = new SpiceManager(HttpClientSpiceService.class);
 
-    ArrayList<ListUserFriendsJson.Friend> friends = new ArrayList<ListUserFriendsJson.Friend>();
     private void rebuildList(final Bundle savedInstanceState) {
         final TimeLineFriendsInviteActivity c = this;
-        adapter = new InviteFriendsListAdapter(savedInstanceState, c, friends);
         ListUserFriendsRequest request = new ListUserFriendsRequest();
         request.setOffset(0);
         request.setLimit(150);
-        manager.execute(request, new TimelineRequestListener<ListUserFriendsJson>(){
+        manager.execute(request, new TimelineRequestListener<ListUserFriendsJson>() {
             @Override
             protected void caseOK(ListUserFriendsJson response) {
+                adapter = new InviteFriendsListAdapter(c, response.getInactiveFriends());
                 //adapter.setOnItemClickListener(this);
-                friends.clear();
-                friends.addAll(response.getInactiveFriends());
-                final ListView listView = c.getListView();
-                adapter.setAdapterView(listView);
+                //adapter.setAdapterView(listView);
+
+                listView.setAdapter(adapter);
                 setFriends(response.getActiveFriends());
+
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                        final MultiChoiceArrayAdapter adapter = (InviteFriendsListAdapter) parent.getAdapter();
-                        adapter.setItemChecked(position, !adapter.isChecked(position));
 
                     }
                 });
@@ -141,50 +131,37 @@ public class TimeLineFriendsInviteActivity extends ActionBarActivity {
         return (ListView) findViewById(android.R.id.list);
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        adapter.save(outState);
-    }
-
     public void setFriends(List<ListUserFriendsJson.Friend> activeFriendsList){
         StringBuilder friendsString;
         int i = 0;
+        String text;
         if(activeFriendsList !=null && !activeFriendsList.isEmpty() ){
-//            int j = i;
-//
-//            do {
-//                friendsString = new StringBuilder(activeFriendsList.get(j).getUsername());
-//                j++;
-//            }while (friendsString.length() == 0);
-//
-//           for(i = j; i<activeFriendsList.size() && i < 3 + j; i++){
-//                String friendName = activeFriendsList.get(i).getUsername();
-//                if(!TextUtils.isEmpty(friendName)){
-//                    friendsString.append(", ").append(friendName);
-//                }
-//            }
+            int j = i;
 
-            String text;
+            do {
+                friendsString = new StringBuilder(activeFriendsList.get(j).getUsername());
+                j++;
+            }while (friendsString.length() == 0);
+
+           for(i = j; i<activeFriendsList.size() && i < 3 + j; i++){
+                String friendName = activeFriendsList.get(i).getUsername();
+                if(!TextUtils.isEmpty(friendName)){
+                    friendsString.append(", ").append(friendName);
+                }
+            }
+
             text = getString(R.string.friends_that_use_timeline);
 
-//            if ( activeFriendsList.size() - i <= 0 ){
-//                text = friendsString.toString() + " " +text;
-//
-//            }else{
-//                text=friendsString.toString()
-//                        +" "+ getString(R.string.and)
-//                        +" "+ String.valueOf(activeFriendsList.size() - i)
-//                        +" "+ getString(R.string.more_friends)
-//                        +" "+ text;
-//            }
-//
-//            }else{
-//                text=friendsString.toString()
-//                        +" "+ getString(R.string.and)
-//                        +" "+ String.valueOf(activeFriendsList.size() - i)
-//                        +" "+ getString(R.string.more_friends)
-//                        +" "+ text;
-//            }
+            if ( activeFriendsList.size() - i <= 0 ){
+                text = friendsString.toString() + " " +text;
+
+            }else{
+                text=friendsString.toString()
+                        +" "+ getString(R.string.and)
+                        +" "+ String.valueOf(activeFriendsList.size() - i)
+                        +" "+ getString(R.string.more_friends)
+                        +" "+ text;
+            }
 
             friends_using_timeline.setText(text);
 
--- unstable/Aptoidev5/branches/Aptoide5.2.1/new_branch/v5engine/src/main/java/cm/aptoide/ptdev/adapters/InviteFriendsListAdapter.java
+++ unstable/Aptoidev5/branches/Aptoide5.2.1/new_branch/v5engine/src/main/java/cm/aptoide/ptdev/adapters/InviteFriendsListAdapter.java
@@ -8,66 +8,43 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CheckedTextView;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import com.manuelpeinado.multichoiceadapter.ItemClickInActionModePolicy;
+import com.manuelpeinado.multichoiceadapter.MultiChoiceAdapter;
 import com.manuelpeinado.multichoiceadapter.extras.actionbarcompat.MultiChoiceArrayAdapter;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.ArrayList;
 
 import cm.aptoide.ptdev.R;
-import cm.aptoide.ptdev.webservices.timeline.json.ListUserFriendsJson;
 import cm.aptoide.ptdev.webservices.timeline.json.ListUserFriendsJson.Friend;
 
 /**
  * Created by asantos   on 09-10-2014.
  */
-public class InviteFriendsListAdapter extends MultiChoiceArrayAdapter<Friend> {
+public class InviteFriendsListAdapter extends ArrayAdapter<Friend> {
     private Context ctx;
 
-    public InviteFriendsListAdapter(Bundle savedInstanceState, Context context, ArrayList<Friend> items) {
-        super(savedInstanceState, context, 0, items);
+    public InviteFriendsListAdapter(Context context, ArrayList<Friend> items) {
+        super(context, 0, items);
         this.ctx = context;
-        setItemClickInActionModePolicy(ItemClickInActionModePolicy.SELECT);
 
-    }
-
-    boolean allSelected;
-
-    public void selectAll() {
-        allSelected = !allSelected;
-        for (int i = 0; i < this.getCount(); ++i) {
-            setItemChecked(i, allSelected);
-        }
     }
 
     @Override
-    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
-
-        return true;
-    }
-
-    @Override
-    public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-        return false;
-    }
-
-    @Override
-    public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-        return true;
-    }
-
-    @Override
-    public View getViewImpl(final int position, View convertView, ViewGroup parent){
+    public View getView(final int position, View convertView, ViewGroup parent){
         final View v;
 	    ViewHolder holder;
 
 	    if(convertView == null){
 		    convertView = LayoutInflater.from(ctx).inflate(R.layout.row_facebook_invite_friends, null);
 			holder = new ViewHolder();
-		    holder.name = (TextView) convertView.findViewById(R.id.username);
+		    holder.name = (CheckedTextView) convertView.findViewById(R.id.username);
 			holder.avatarImage = (ImageView) convertView.findViewById(R.id.user_avatar);
 
 		    convertView.setTag(holder);
@@ -76,15 +53,21 @@ public class InviteFriendsListAdapter extends MultiChoiceArrayAdapter<Friend> {
 	    }
         v = convertView;
 
-        ListUserFriendsJson.Friend friend = getItem(position);
+        Friend friend = getItem(position);
 
+        holder.name.setChecked(((ListView)parent).isItemChecked(position));
 	    holder.name.setText(friend.getUsername());
         ImageLoader.getInstance().displayImage(friend.getAvatar(), holder.avatarImage);
         return v;
     }
 
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
     public static class ViewHolder{
-        public TextView name;
+        public CheckedTextView name;
         public ImageView avatarImage;
     }
 }
