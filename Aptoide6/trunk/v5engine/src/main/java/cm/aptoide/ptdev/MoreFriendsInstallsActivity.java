6a53119c235f152a445687364f6a1ce53a0ecea3
andre.santos <andre.santos@8d55eb69-6847-4b75-98a7-935b3913b0ec>
2014-12-01 17:21:04 +0000

    git-svn-id: http://aptoide.org/repo/aptoide-client@2335 8d55eb69-6847-4b75-98a7-935b3913b0ec

---
+++ Aptoide6/trunk/v5engine/src/main/java/cm/aptoide/ptdev/MoreFriendsInstallsActivity.java
@@ -0,0 +1,95 @@
+package cm.aptoide.ptdev;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.octo.android.robospice.SpiceManager;
+import com.octo.android.robospice.persistence.DurationInMillis;
+import com.octo.android.robospice.persistence.exception.SpiceException;
+import com.octo.android.robospice.request.listener.RequestListener;
+
+import cm.aptoide.ptdev.services.HttpClientSpiceService;
+import cm.aptoide.ptdev.webservices.timeline.ListApksInstallsRequest;
+import cm.aptoide.ptdev.webservices.timeline.json.TimelineListAPKsJson;
+
+/**
+ * Created by asantos on 01-12-2014.
+ */
+public class MoreFriendsInstallsActivity extends ActionBarActivity {
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home || item.getItemId() == R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Aptoide.getThemePicker().setAptoideTheme(this);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_store);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle(R.string.notification_timeline_posts);
+
+        Fragment fragment = new MoreFriendsInstallsFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+    }
+
+    public static class MoreFriendsInstallsFragment extends Fragment {
+        private RecyclerView view;
+        SpiceManager spiceManager = new SpiceManager(HttpClientSpiceService.class);
+        @Override
+        public void onStart() {
+            super.onStart();
+            spiceManager.start(getActivity());
+        }
+        @Override
+        public void onStop() {
+            super.onStop();
+            spiceManager.shouldStop();
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_list_apps, container, false);
+
+            view = (RecyclerView) rootView.findViewById(R.id.list);
+
+            return rootView;
+
+
+        }
+
+        @Override
+        public void onViewCreated(View view, Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            ListApksInstallsRequest listRelatedApkRequest = new ListApksInstallsRequest();
+
+            spiceManager.execute(listRelatedApkRequest, "MoreFriendsInstallsFragment", DurationInMillis.ONE_DAY, new RequestListener<TimelineListAPKsJson>() {
+                @Override
+                public void onRequestFailure(SpiceException spiceException) {
+
+                }
+
+                @Override
+                public void onRequestSuccess(TimelineListAPKsJson timelineListAPKsJson) {
+
+                }
+            });
+        }
+    }
+}
